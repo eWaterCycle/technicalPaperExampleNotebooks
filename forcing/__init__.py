dbf9a08fb64dd6ed809eb5812598fdd19f208a71
Stef Smeets <s.smeets@esciencecenter.nl>
2021-03-22 13:29:33 +0100

    Set up scaffolding

---
+++ forcing/__init__.py
@@ -0,0 +1,8 @@
+def generate(*, 
+    forcing: str='ERA5',
+    model: str='Marrmot', 
+    startyear: int=2012, 
+    endyear: int=2020, 
+    catchment: str='/some/file.shp',
+        ):
+    raise NotImplementedError
